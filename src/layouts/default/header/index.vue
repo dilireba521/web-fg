ec73f264b9ae3417a7edbf949c97681eb48d7ef7
lkh <215551728@qq.com>
2025-04-01 14:44:47 +0800

    feat: 全局样式

--- src/layouts/default/header/index.vue
+++ src/layouts/default/header/index.vue
@@ -1,3 +1,10 @@
 <template>
     <div class="header">header</div>
 </template>
+<script lang="ts" setup></script>
+<style lang="less" scoped>
+.header {
+    height: 60px;
+    color: @colorPrimary1;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { configSvgIconsPlugin } from './vite-config/svgSprite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  plugins: [vue(), vueJsx(), UnoCSS(), vueDevTools()],
+  plugins: [vue(), vueJsx(), UnoCSS(), vueDevTools(), configSvgIconsPlugin({ isBuild: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
